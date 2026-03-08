33127dae9843caaa129a578cd3b7afe4899d6e82
Hammada <hammadaballali@gmail.com>
2026-03-08 13:03:56 +0000

    Update GitHub Actions yml

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
       <Home
         name="Hammada Ballali"
         title="Software Engineer"
-        profilePhoto="images\profileImage.jpg"
+        profilePhoto="images/profileImage.jpg"
       />
       <About
         description="Experienced IT professional and PhD candidate in Computer Vision with a strong foundation in machine learning and data analysis. Proficient in designing and implementing Python-based solutions, developing ML models, and optimizing workflows for data-driven applications. Skilled in leveraging Python libraries such as TensorFlow, PyTorch, and scikit-learn to build, train, and deploy models for predictive analytics, computer vision, and automation. Experienced in deploying applications on Windows and Ubuntu servers, managing AWS services, and integrating machine learning pipelines into cloud environments. Adept at automating processes using Python scripts and Docker, with hands-on experience in tools like Kafka and Kubernetes. Passionate about AI and machine learning, with a focus on advancing cutting-edge technologies in computer vision to deliver innovative and impactful solutions. I also enjoy working with the Django–React–Tailwind stack."
--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  base: '/Hammada-Ballali-Web-Portfolio/' // <-- CHANGE THIS to your repo name
 })
